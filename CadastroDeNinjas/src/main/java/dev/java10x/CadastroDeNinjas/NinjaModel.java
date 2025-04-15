06c67584f78bc496c208977935d0f82b1b785ec3
christiano02 <christianonascimento02@hotmail.com>
2025-04-14 22:56:48 -0300

    remove a pasta .idea do repositorio

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -9,13 +9,46 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel(){
 
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public NinjaModel(final String nome, final String email, final int idade){
@@ -25,4 +58,5 @@ public class NinjaModel {
         this.idade = idade;
 
     }
+
 }
